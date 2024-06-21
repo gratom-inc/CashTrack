f792520c038c4ff3f6a74e7937519de529126f64
Arjun G. Menon <contact@arjungmenon.com>
2024-06-20 22:13:29 -0400

    Experimenting with BigDecimal wrappers

--- MoneyTrack/src/main/kotlin/Main.kt
+++ MoneyTrack/src/main/kotlin/Main.kt
@@ -1,6 +1,8 @@
 import assets.schwab.SchwabCheckingData
 import assets.schwab.parseDate
 import assets.schwab.readSchwabCheckingCsv
+import com.github.labai.deci.Deci
+import io.github.rtmigo.dec.Dec
 
 val schwabCheckingCutoffDateA = parseDate("05/01/2022") // parseDate("10/23/2020")
 
@@ -26,6 +28,13 @@ fun main(args: Array<String>) {
         println(it)
     }
     println("\nProcessing took $timeTaken ms")
+
+    val x1 = listOf<Deci>(Deci(1), Deci(2), Deci(3))
+    val x2 = listOf<Dec>(Dec(1), Dec(2), Dec(3))
+//    val x1s = x1.sumOf { it }
+//    val x2s = x2.sumOf { it }
+    println("x1: ${x1}")
+    println("x2: ${x2}")
 
     // println("Hello World!")
     // Try adding program arguments via Run/Debug configuration.
