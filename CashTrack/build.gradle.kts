c77d15b01f7a58119bb3ed0f058c6113d58f0e61
Arjun G. Menon <contact@arjungmenon.com>
2024-06-25 01:31:13 -0400

    Merge server config into Application.kt

--- CashTrack/build.gradle.kts
+++ CashTrack/build.gradle.kts
@@ -2,6 +2,7 @@ import cz.habarta.typescript.generator.JsonLibrary
 import cz.habarta.typescript.generator.TypeScriptFileType
 import cz.habarta.typescript.generator.TypeScriptOutputKind
 
+val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 
@@ -29,6 +30,7 @@ repositories {
 dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-jetty-jvm")
+    implementation("io.ktor:ktor-server-status-pages")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
--- CashTrack/src/main/kotlin/com/gratom/Application.kt
+++ CashTrack/src/main/kotlin/com/gratom/Application.kt
@@ -1,16 +1,31 @@
 package com.gratom
 
-import com.gratom.server.plugins.*
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.jetty.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun main() {
-    embeddedServer(Jetty, port = 7178, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
-}
-
-fun Application.module() {
-    configureAdministration()
-    configureRouting()
+    embeddedServer(Jetty, port = 7178, host = "0.0.0.0") {
+        install(StatusPages) {
+            exception<Throwable> { call, cause ->
+                call.respondText(text = "500: $cause" , status = HttpStatusCode.InternalServerError)
+            }
+            status(HttpStatusCode.NotFound) { call, status ->
+                call.respondText(text = "404: Not Found", status = status)
+            }
+        }
+        install(ShutDownUrl.ApplicationCallPlugin) {
+            shutDownUrl = "/shutdown"
+            exitCodeSupplier = { 0 }
+        }
+        routing {
+            get("/hello") {
+                call.respondText("Hello: ${foo()}")
+            }
+        }
+    }.start(wait = true)
 }
